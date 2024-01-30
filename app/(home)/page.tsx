9239f812a71b3f672810a8c43bfaa3fa14a741e3
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-01-30 17:29:24 -0300

    Feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
 import Header from "../_components/Header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
    return (
       <div>
          <Header />
+
+         <div className="px-5 pt-5">
+            <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+            <p className="capitalize text-sm">
+               {format(new Date(), "EEEE',' d 'de' MMMM", {
+                  locale: ptBR,
+               })}
+            </p>
+         </div>
       </div>
    );
 }
