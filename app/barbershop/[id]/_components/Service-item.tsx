126cae27cace2bc8d2332697775b3b500e07b73b
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-01 17:56:54 -0300

    Feat; add toast on booking success

--- app/barbershop/[id]/_components/Service-item.tsx
+++ app/barbershop/[id]/_components/Service-item.tsx
@@ -20,6 +20,8 @@ import { generateDayTimeList } from "../_helpers/hours";
 import { format, setHours, setMinutes } from "date-fns";
 import { saveBooking } from "../_actions/Save-booking";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface ServiceItemProps {
    barbershop: Barbershop;
@@ -32,12 +34,15 @@ const ServiceItem = ({
    isAuthenticated,
    barbershop,
 }: ServiceItemProps) => {
+   const router = useRouter();
+
    const { data } = useSession();
 
    const [date, setDate] = useState<Date | undefined>(undefined);
    const [hour, setHour] = useState<string | undefined>();
 
    const [submitIsLoading, setSubmitIsLoading] = useState(false);
+   const [sheetIsOpen, setSheetIsOpen] = useState(false);
 
    const handleDateClick = (date: Date | undefined) => {
       setDate(date);
@@ -72,6 +77,23 @@ const ServiceItem = ({
             barbershopId: barbershop.id,
             date: newDate,
             userId: (data.user as any).id,
+         });
+
+         setSheetIsOpen(false);
+         setHour(undefined);
+         setDate(undefined);
+         toast("Reserva realizada com sucesso", {
+            description: format(
+               newDate,
+               "'Para' dd 'de' MMMM 'as' HH':'mm'.'",
+               {
+                  locale: ptBR,
+               }
+            ),
+            action: {
+               label: "Vizualizar",
+               onClick: () => router.push("/bookings"),
+            },
          });
       } catch (error) {
          console.log(error);
@@ -109,7 +131,7 @@ const ServiceItem = ({
                            currency: "BRL",
                         }).format(Number(service.price))}
                      </p>
-                     <Sheet>
+                     <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
                         <SheetTrigger asChild>
                            <Button
                               variant="secondary"
