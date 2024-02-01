983db18833d479d6c029cd3a546e7cd7cf6ba0ad
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-01 09:37:20 -0300

    Feat: add auth provider

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+   const { data } = useSession();
+
+   const handleLoginClick = async () => {
+      await signIn();
+   };
+
    return (
       <Card>
          <CardContent className="flex flex-row justify-between p-5 items-center">
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/Footer";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
    children: React.ReactNode;
 }>) {
    return (
-      <html lang="en">
+      <html lang="pt-br">
          <body className={`${inter.className} dark`}>
-            {children}
-            <Footer />
+            <AuthProvider>
+               {children}
+               <Footer />
+            </AuthProvider>
          </body>
       </html>
    );
