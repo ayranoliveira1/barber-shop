992f428268b7dcc259166261ae771731212654a8
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-12 14:29:24 -0300

    Feat: add contact us and transparency in the footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,9 +1,37 @@
+import { Mail, Phone } from "lucide-react";
+
 const Footer = () => {
    return (
       <footer className="w-full bg-secondary py-6 px-5">
-         <p className="text-gray-400 text-xs font-bold opacity-75">
-            © 2023 Copyright FSW Barber
-         </p>
+         <div className="flex justify-between px-6">
+            <div className="flex flex-col">
+               <p className="text-xl text-gray-300 ">Fale conosco</p>
+
+               <div className="flex">
+                  <Phone className=" text-gray-400 text-xs pr-1 pt-2" />
+                  <p className="pt-2 text-gray-400 text-xs font-bold opacity-75">
+                     +55 32 98434-1504
+                  </p>
+               </div>
+
+               <div className="flex">
+                  <Mail className=" text-gray-400 text-xs pr-1 pt-2" />
+                  <p className="pt-2 text-gray-400 text-xs font-bold opacity-75">
+                     ayranoliveira1@gmail.com
+                  </p>
+               </div>
+            </div>
+
+            <div className="flex flex-col">
+               <p className="text-xl text-gray-300 ">Transparência</p>
+               <p className="pt-2 text-gray-400 text-xs font-bold opacity-75">
+                  Termos de uso
+               </p>
+               <p className="pt-2 text-gray-400 text-xs font-bold opacity-75">
+                  Política de privacidade
+               </p>
+            </div>
+         </div>
       </footer>
    );
 };
