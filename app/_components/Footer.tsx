14d22e9f05059c1b08f5bcdcb29b27fbeca1a118
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-14 14:48:57 -0300

    Refactor: add padding in footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,10 +2,10 @@ import { Mail, Phone } from "lucide-react";
 
 const Footer = () => {
    return (
-      <footer className="w-full bg-secondary py-6 px-5">
+      <footer className="w-full bg-secondary pt-2 pb-4 px-5">
          <div className="flex justify-between px-6">
             <div className="flex flex-col">
-               <p className="text-xl text-gray-300 ">Fale conosco</p>
+               <p className="text-lg text-gray-300 ">Fale conosco</p>
 
                <div className="flex">
                   <Phone className=" text-gray-400 text-xs pr-1 pt-2" />
@@ -23,7 +23,7 @@ const Footer = () => {
             </div>
 
             <div className="flex flex-col">
-               <p className="text-xl text-gray-300 ">Transparência</p>
+               <p className="text-lg text-gray-300 ">Transparência</p>
                <p className="pt-2 text-gray-400 text-xs font-bold opacity-75">
                   Termos de uso
                </p>
