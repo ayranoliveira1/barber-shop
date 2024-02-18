4f7cb360748a26c35613844701ae9b861f27e15b
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-18 17:59:02 -0300

    Feat: add responsiveness to the booking page

--- app/barbershop/page.tsx
+++ app/barbershop/page.tsx
@@ -28,18 +28,20 @@ const BarbershopPage = async ({ searchParams }: BarbershopPageProps) => {
       <>
          <Header />
 
-         <div className="px-5 py-6 flex flex-col gap-6">
-            <Search
-               defaultValues={{
-                  search: searchParams.search,
-               }}
-            />
+         <div className="px-5 py-6 flex flex-col xl:w-[86%] xl:mx-auto gap-6">
+            <div className="xl:hidden">
+               <Search
+                  defaultValues={{
+                     search: searchParams.search,
+                  }}
+               />
+            </div>
 
             <h1 className="text-gray-400 font-bold text-xs uppercase">
                Resultados para &quot;{searchParams.search}&quot; ;
             </h1>
 
-            <div className="grid grid-cols-2 mt-3 gap-4">
+            <div className="grid grid-cols-2 xl:grid-cols-6 mt-3 gap-4">
                {barbershops.map((barbershop: any) => (
                   <div className="w-full" key={barbershop}>
                      <BarberShopItem barbershop={barbershop} />
