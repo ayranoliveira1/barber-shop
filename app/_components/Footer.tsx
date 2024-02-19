d76866fa79aee8fdd5d42569f1a9cb59785249f4
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-02-19 17:41:49 -0300

    Feat: add information and images to the footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,11 +3,31 @@ import Image from "next/image";
 
 const Footer = () => {
    return (
-      <footer className="flex justify-center w-full bg-secondary pt-5 pb-5 px-3">
-         <div className="flex flex-col gap-4 px-6">
+      <footer className="bg-secondary py-5 px-3 xl:px-20">
+         <div className="flex flex-col md:flex md:flex-row md:justify-around md:gap-4 gap-4 px-6">
             <div className="flex justify-center">
                <p className="font-bold text-2xl">
                   <Image src="/logo.png" alt="Barber" width={170} height={22} />
+               </p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+               <p className="text-xl text-gray-300 text-center">BarberShop</p>
+               <p className=" text-gray-400 text-md text-center">
+                  Dúvidas frequentes
+               </p>
+               <p className=" text-gray-400 text-md text-center">Clientes</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+               <p className="text-xl text-gray-300 text-center">
+                  Transparência
+               </p>
+               <p className=" text-gray-400 text-md text-center">
+                  Termos de uso
+               </p>
+               <p className=" text-gray-400 text-md text-center">
+                  Política de privacidade
                </p>
             </div>
 
@@ -29,13 +49,23 @@ const Footer = () => {
 
             <div className="flex flex-col gap-2">
                <p className="text-xl text-gray-300 text-center">
-                  Transparência
+                  Baixe nosso App
                </p>
-               <p className=" text-gray-400 text-md text-center">
-                  Termos de uso
+               <p className="flex justify-center text-gray-400 text-md text-center">
+                  <Image
+                     src="/appstore.png"
+                     alt="App store"
+                     width={120}
+                     height={50}
+                  />
                </p>
-               <p className=" text-gray-400 text-md text-center">
-                  Política de privacidade
+               <p className="flex justify-center text-gray-400 text-md text-center">
+                  <Image
+                     src="/googleplay.png"
+                     alt="App store"
+                     width={120}
+                     height={50}
+                  />
                </p>
             </div>
          </div>
