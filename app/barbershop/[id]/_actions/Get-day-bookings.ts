cc9071408ed8abb1c0dd9a05bd0d91e404e5f168
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-08-22 21:06:05 -0300

    feat: add get day bookings request

--- app/barbershop/[id]/_actions/Get-day-bookings.ts
+++ app/barbershop/[id]/_actions/Get-day-bookings.ts
@@ -3,16 +3,17 @@
 import { db } from "@/app/_lib/prisma";
 import { endOfDay, startOfDay } from "date-fns";
 
+// getDayBookings
 export const getDayBookings = async (barbershopId: string, date: Date) => {
-   const bookings = await db.booking.findMany({
-      where: {
-         barbershopId,
-         date: {
-            lte: endOfDay(date),
-            gte: startOfDay(date),
-         },
+  const bookings = await db.booking.findMany({
+    where: {
+      barbershopId,
+      date: {
+        lte: endOfDay(date),
+        gte: startOfDay(date),
       },
-   });
+    },
+  });
 
-   return bookings;
+  return bookings;
 };
