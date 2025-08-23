39a1d4e9977768b0c6ef190175cf9e7a26a86444
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-08-22 21:06:28 -0300

    feat: add save bookin request

--- app/barbershop/[id]/_actions/Save-booking.ts
+++ app/barbershop/[id]/_actions/Save-booking.ts
@@ -4,22 +4,23 @@ import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
 
 interface SaveBookingParams {
-   barbershopId: string;
-   serviceId: string;
-   userId: string;
-   date: Date;
+  barbershopId: string;
+  serviceId: string;
+  userId: string;
+  date: Date;
 }
 
+// saveBooking
 export const saveBooking = async (params: SaveBookingParams) => {
-   await db.booking.create({
-      data: {
-         serviceId: params.serviceId,
-         userId: params.userId,
-         date: params.date,
-         barbershopId: params.barbershopId,
-      },
-   });
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      userId: params.userId,
+      date: params.date,
+      barbershopId: params.barbershopId,
+    },
+  });
 
-   revalidatePath("/");
-   revalidatePath("/bookings");
+  revalidatePath("/");
+  revalidatePath("/bookings");
 };
